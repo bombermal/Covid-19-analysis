9e078971cad8742f7911aaa034e8fbf2cd9e84b4
Ivan Alisson <ivanalisson@hotmail.com>
2020-05-15 16:39:18 -0300

    Finished terminal version

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3-alpine
 RUN apk add build-base freetype-dev
 RUN pip install biopython tqdm pandas numpy matplotlib
 COPY ["main_terminal.py", "/" ]
-COPY ./Codes/*.py /Codes
+COPY ./Codes/*.py Codes/
 
 CMD ["/bin/sh"]
--- main_terminal.py
+++ main_terminal.py
@@ -34,87 +34,82 @@ def read_files(path):
     path = "/".join(aux[:-1])+"/"
     name = aux[-1]
     raw_aln = rdF.read_aligned_files(path, name)
-	#Converto alinhado para um DF
+ 	#Converto alinhado para um DF
     df_aln = rdF.seq_to_df(raw_aln)
     
     return df_aln
 
-def default_name(source=""):
+def default_name(source, csv = False):
     """
-    Gera um nome padrtão para as amostras, caso não seja dado um nome
+    Define um nome para os arquivos gerados, tanto para o csv quanto para o png
 
     Parameters
     ----------
-    plot : TYPE, optional
-        DESCRIPTION. The default is True.
+    source : str
+        Path do arquivo fonte, usado para o processamento
+    csv : bool, optional
+        Determina se será criado o nome para um png ou para um scv. The default is False.
 
     Returns
     -------
-    TYPE
-        DESCRIPTION.
+    str
+        o nome para o arquivo que será salvo. 
 
     """
     # Pega apenas o nome do arquivo passando no caminho: exemplo/pasta/arquivo.csv -> arquivo
     source = source.split("/")[-1].split(".")[0]+"_"
     # Cria um nome único usando a data atual > Resultado_01-01-01_12-12-12
     date = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-    result = "Resultado_"
-    """
-        Preciso revisar
-    """
-    if source == "":
-        return result+date+".csv"
+    if csv:
+        return "Counted_df_"+source+date+".csv"
     else:
-        return result+source+date
+        return "Resultado_"+source+date
 
 
-def create(df_aln, source, target=default_name(), read=True):
+def create(df, source, target):
     """
-    Lê arquivo de contagem já pronto ou cria um
+    Fata a contagem dos SNPs nas sequencias e salva em csv
 
     Parameters
     ----------
-    df_aln : TYPE
-        DESCRIPTION.
-    target : TYPE, optional
-        DESCRIPTION. The default is default_name().
-    read : TYPE, optional
-        DESCRIPTION. The default is True.
+    df : Dataframe
+        DataFrame com as sequências para a análise 
+    source : str
+        nome que será utilizado no arquivo csv
+    target : str
+        caminho onde o csv será salvo
 
     Returns
     -------
-    counted_df : TYPE
-        DESCRIPTION.
+    counted_df : DataFrame
+        Resultado da contagem de SNPs
 
     """
-    if read:
-        #lê salvo
-        counted_df = pd.read_csv(source)
-    else:
-        #Df vazio
-        counted_df = pd.DataFrame()
-        #função que faz a transposta de Seq e conta as ocorrencias
-        fc.transpose_seq_and_count(df_aln.Seq, counted_df)
-        #Limpa os Nan e corrige a Pos
-        counted_df = counted_df.fillna(0).reset_index().rename(columns={"index" : "Pos"})
-        counted_df.Pos = counted_df.Pos.apply(lambda x: x+1)
-        #Salva o trabalho
-        counted_df.to_csv(target+default_name(), index=False)
+    #Df vazio
+    counted_df = pd.DataFrame()
+    #função que faz a transposta de Seq e conta as ocorrencias
+    fc.transpose_seq_and_count(df.Seq, counted_df)
+    #Limpa os Nan e corrige a Pos
+    counted_df = counted_df.fillna(0).reset_index().rename(columns={"index" : "Pos"})
+    counted_df.Pos = counted_df.Pos.apply(lambda x: x+1)
+    #Salva o trabalho
+    counted_df.to_csv(target+default_name(source, True), index=False)
     
     return counted_df
 
 
 def docker_flow():
     """
-    Work flow do trabalho preparado para terminal e Docker
+    Cordena todo o processo de leitura do multifasta, criação da tabela de SNPs e png do gráfico
 
     Returns
     -------
     int
-        DESCRIPTION.
+        0, para finalizar o programa
 
     """
-    #Parser
+    
+    # Argparse
     parser = argparse.ArgumentParser(description="Plot de SNPs")
     parser.add_argument("--option", "-o", required=True, help="ler/criar - se igual a 'ler', recebe um csv já processado para gerar o grafico, se igual a 'criar' recebe fasta, e retorna um csv e um gráfico de SNPs")
     parser.add_argument("--source", "-s", required=True, help="Endereço do arquivo fonte")
@@ -125,23 +120,27 @@ def docker_flow():
     args = parser.parse_args()
     
     if args.option == "ler":
+        #Ler CSV já processado
         counted_df = pd.read_csv(args.source)
     else:
-        #Ler aqrquivos
+        #Ler multifasta
         df_samples = read_files(args.source)
-        counted_df = create(df_samples, target=args.target, read=False)
-    
-    #Plotar
-    ## Melhorar o filtro para receber quantidade variável de valores
-    ## Atual: 3 valores float
-    filter = args.filter
-    list_of_filtered_dfs = []
-    
-    for ii in conditions:
-        list_of_filtered_dfs.append(fc.filter_criteria(counted_df, counted_df.loc[0].sum()-1, ii))
-    	    
-    gf.three_plots(filter, list_of_filtered_dfs, default_name(args.source), target=args.target, dpi=args.dpi)
+        # Cria DF com as posições contadas
+        counted_df = create(df_samples, source=args.source, target=args.target)
         
+        
+    # Processar
+    # valores usados como filtro, passados no terminal
+    filter = args.filter
+    #Lista onde será armazenado os DataFrames com os valores filtrados
+    list_of_filtered_dfs = []
+
+    for ii in filter:
+        list_of_filtered_dfs.append(fc.filter_criteria(counted_df, counted_df.loc[0].sum()-1, ii))
+ 
+    # Gerar gráficos
+    gf.three_plots(filter, list_of_filtered_dfs, default_name(args.source), target=args.target, dpi=args.dpi)
+    
     return 0
 
 sys.exit(docker_flow())
